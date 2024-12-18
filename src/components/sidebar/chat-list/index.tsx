ff620fc7191c6aba2a62b4758883f17a31503cf6
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-12-18 11:27:55 +0545

    feat: make user list

--- src/components/sidebar/chat-list/index.tsx
+++ src/components/sidebar/chat-list/index.tsx
@@ -3,6 +3,8 @@ import { HiPlus } from 'react-icons/hi'
 import { HiMinus } from 'react-icons/hi'
 import { IoSearch } from 'react-icons/io5'
 
+import ChatItem from './chat-item'
+
 const ChatList = () => {
   const [name, setName] = useState('')
   const [addMode, setAddMode] = useState(true)
@@ -10,23 +12,32 @@ const ChatList = () => {
   const toggleModes = () => setAddMode(prevState => !prevState)
 
   return (
-    <div className="flex items-center justify-between p-4">
-      <div className="relative">
-        <IoSearch className="absolute left-2 top-1/2 -translate-y-1/2" />
-        <input
-          className="inset-0 w-full rounded-lg bg-primary p-2 pl-8"
-          type="text"
-          value={name}
-          onChange={e => setName(e.target.value)}
-        />
+    <section>
+      <div className="flex items-center justify-between p-4">
+        <div className="relative">
+          <IoSearch className="absolute left-2 top-1/2 -translate-y-1/2" />
+          <input
+            className="inset-0 w-full rounded-lg bg-primary p-2 pl-8"
+            type="text"
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+        </div>
+        <button
+          className="grid size-10 cursor-pointer place-items-center rounded-lg bg-primary text-xl duration-300 active:scale-90"
+          onClick={toggleModes}
+        >
+          {addMode ? <HiPlus /> : <HiMinus />}
+        </button>
       </div>
-      <button
-        className="grid size-10 cursor-pointer place-items-center rounded-lg bg-primary text-xl duration-300 active:scale-90"
-        onClick={toggleModes}
-      >
-        {addMode ? <HiPlus /> : <HiMinus />}
-      </button>
-    </div>
+      <div className="flex flex-col divide-y divide-white divide-opacity-30 border-b border-white/30">
+        {Array(4)
+          .fill(0)
+          .map((_, index) => (
+            <ChatItem key={index} />
+          ))}
+      </div>
+    </section>
   )
 }
 
